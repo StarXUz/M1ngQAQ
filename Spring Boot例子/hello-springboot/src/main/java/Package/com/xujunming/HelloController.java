f438331604ef16b48a2cd0da1a12d58b659d0e0f
Junming Xu <xujunming123w@gmail.com>
2026-01-12 01:09:32 +0800

    Add files via upload

---
+++ Spring Boot例子/hello-springboot/src/main/java/Package/com/xujunming/HelloController.java
@@ -0,0 +1,13 @@
+package com.xujunming.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+}
